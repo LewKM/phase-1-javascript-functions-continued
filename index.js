0ba2840111fe81275ae2084d2a69d2210b2a5a72
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-06-07 12:23:19 +0300

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 function saturdayFun(activity = "roller-skate") {
     return (`This Saturday, I want to ${activity}!`);
 }
-saturdayFun()
-saturdayFun("bathe my dog")
+saturdayFun();
+saturdayFun("bathe my dog");
 
 function mondayWork(routine = "go to the office") {
     return (`This Monday, I will ${routine}.`);
 }
+mondayWork();
 
 function wrapAdjective(cool = "*") {
     return function(message = "special") {
         return `You are ${cool}${message}${cool}!`;
     }
 }
+wrapAdjective("||");
