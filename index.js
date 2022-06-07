e96385dd4430a7ba101c18d465a1a1586427aa8c
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-06-07 12:18:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(routine = "go to the office") {
+    return (`This Monday, I will ${routine}.`);
+}
+
+function wrapAdjective(cool = "*") {
+    return function(message = "a hard worker") {
+        return `You are ${cool}${message}${cool}!`;
+    }
+}
